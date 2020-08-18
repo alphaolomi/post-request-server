315075fa4bcf0ee46714adc61de2e10c3278ab8d
Alpha Olomi <alphaolomi@gmail.com>
2020-08-18 11:03:57 +0300

    refactor seeds

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,45 +1,18 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $post = new Post();
-        $post->title = "Post 1";
-        $post->description = "This is post 1";
-        $post->save();
+        for ($i = 0; $i < 6; $i++) {
 
-        $post = new Post();
-        $post->title = "Post 2";
-        $post->description = "This is post 2";
-        $post->save();
-
-        $post = new Post();
-        $post->title = "Post 3";
-        $post->description = "This is post 3";
-        $post->save();
-
-        $post = new Post();
-        $post->title = "Post 4";
-        $post->description = "This is post 4";
-        $post->save();
-
-        $post = new Post();
-        $post->title = "Post 5";
-        $post->description = "This is post 5";
-        $post->save();
-
-        $post = new Post();
-        $post->title = "Post 6";
-        $post->description = "This is post 6";
-        $post->save();
+            factory(App\Post::class)->create([
+                'title' => "Post ${i}",
+                'description' => "This is post ${i}",
+                'imagePath' => 'image.jpg'
+            ]);
+        }
     }
 }
